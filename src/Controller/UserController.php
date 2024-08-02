940afb8c0d755faec34799be6b3fd90044156c2a
Jesus Mejia <jesussft95@gmail.com>
2024-08-02 01:50:39 +0100

    login finish

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,15 +1,17 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-
 use App\Entity\User;
 use App\Form\RegisterType;
-
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
 
 class UserController extends AbstractController
 {
@@ -29,28 +31,19 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             // Modificar el objeto para agregar el rol 
             $user->setRole('ROLE_USER');
-            
-            //var_dump($date_now);
-
-
-
             $user->setCreateAt(new \Datetime('now'));
             
             // CIFRAR CONTRASEÑA Y GUARDAR EN OBJETO 
             $encodedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
             $user->setPassword($encodedPassword);
 
-
-            var_dump($user);
-
             // Persistir y guardar el usuario
-           
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
 
             // Redirigir o mostrar mensaje
-           return $this->redirectToRoute('tasks'); // Cambia 'some_route' por la ruta deseada
+            return $this->redirectToRoute('tasks'); // Cambia 'tasks' por la ruta deseada
         }
 
         // Renderizar el formulario
@@ -58,4 +51,19 @@ class UserController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/login", name="user_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+        ]);
+    }
+
 }

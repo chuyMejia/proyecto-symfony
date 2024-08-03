d4c2d1c4b039db2f78510be594a6afe88449d281
Jesus Mejia <jesussft95@gmail.com>
2024-08-03 02:03:54 +0100

    taskqueño

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -8,6 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Task;
 use App\Entity\User;
 
+
+use Symfony\Component\HttpFoundation\Request;
+
 class TaskController extends AbstractController
 {
    
@@ -19,37 +22,73 @@ class TaskController extends AbstractController
 
         $em =$this->getDoctrine()->getManager();
         
-        /*
+        
         $task_repo =$this->getDoctrine()->getRepository(Task::class);
-        $tasks =   $task_repo->findAll();
-
+        $tasks =   $task_repo->findBy([],['id' => 'DESC']);
+        /*
           foreach($tasks as $task ){
             echo $task->getUser()->getEmail().':'.$task->getTitle()."</br>";
 
           }
         */
 
-        $user_repo = $this->getDoctrine()->getRepository(User::class);
+       /* $user_repo = $this->getDoctrine()->getRepository(User::class);
         $users = $user_repo->findAll();
-
         foreach($users as $user ){
-
-
             echo "<h1>{$user->getName()} {$user->getSurname()}</h1>";
             foreach($user->getTasks() as $task){
-
-
                 echo  $task->getTitle()."<br/>";
-
             }
-
-        }
+        }*/
 
 
 
 
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks' => $tasks,
         ]);
     }
+
+     /**
+ * @Route("/tarea/{id}", name="task_detail")
+ */
+public function detail2(Task $task = null)
+{
+    if (!$task) {
+        throw $this->createNotFoundException('Task not found');
+    }
+
+    return $this->render('task/detail.html.twig', [
+        'task' => $task
+    ]);
+}
+
+
+
+public function detail($id){
+
+    //load repository
+
+    $task_repo = $this->getDoctrine()->getRepository(Task::class);
+    //cosulta
+    $task = $task_repo->find($id);
+
+    //cpmprapbar result
+
+    if (!$task) {
+        throw $this->createNotFoundException('Task not found');
+    }
+
+
+   
+
+
+    return $this->render('task/detail.html.twig', [
+        'task' => $task
+    ]);
+
+}
+
+
+
 }
